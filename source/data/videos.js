2885759957d2da07eab5f66aff381052c61244c7
Denis Merigoux <denis.merigoux@gmail.com>
2016-01-05 17:19:30 +0100

    Added fake projection

--- source/data/videos.js
+++ source/data/videos.js
@@ -1,6 +1,77 @@
 var videos = {
-    "intro" : {
-        "pathPrefix" : "../../../",
-        "src" : "videos/intro.mp4"
-    }
+        "intro" : {
+            "pathPrefix" : "../../../videos/",
+            "src" : "intro.mp4"
+        },
+        "interviews" : {
+            "videoPathPrefix" : "../../../videos/2_Interviews/",
+            "posterPathPrefix" : "../../../images/miniatures/",
+            "videos" : [
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                },
+                {
+                    "poster" : "2-01_Jacques_Biot.png",
+                    "src" :  "1_Jacques_Biot.mp4",
+                    "title" : "Jacques Biot"
+                }
+            ]
+        }
 }
