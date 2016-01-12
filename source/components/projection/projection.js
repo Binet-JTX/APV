d228f4f5debff894b84bfc4c286d1131bda56e3b
Denis Merigoux <denis.merigoux@gmail.com>
2016-01-12 11:53:37 +0100

    Slides working, CSS still broken

--- source/components/projection/projection.js
+++ source/components/projection/projection.js
@@ -16,12 +16,17 @@ projectionDisplay.pathPrefix = display.projection.pathPrefix;
 
 //pure.js directive to fill in the video sources and posters
 var videosDirective = {
-    '.projection-video': {
-        'video<-videos': {
-            '.projection-video-poster@src': '#{posterPathPrefix}/#{video.poster}',
-            '.projection-video-title': 'video.title',
+    '.slidemenu' : {
+        'slide<-videos' : {
+            '.projection-video': {
+                'video<-slide': {
+                    '.projection-video-poster@src': '#{posterPathPrefix}/#{video.poster}',
+                    '.projection-video-title': 'video.title',
+                }
+            }
         }
     }
+
 };
 
 var displayDirective = {
@@ -82,76 +87,8 @@ var goTo = function(i) {
 }
 
 
-
-/*---------------------------------------------------
-
-                Bloc de navigation
-
-----------------------------------------------------*/
-var baseNumeroVideos = 2000;
-var activeIntro = false;
-var activeTeam = false;
-
-var hasIntro = [2, 5, 13, 15, 17, 21, 23, 25, 27, 29, 31, 33, 35, 38, 40, 42, 44, 46, 49, 55];
-var inTeam = [ //inTeam[i] donne la page de Team pour acceder a i
-    22, -1, 0, 8, -1, 0, 0, 0, 0, 1, 1,
-    9, -1, 16, -1, 10, -1, 17, 18, 17, -1,
-    11, -1, 19, -1, 11, -1, 20, -1, 11, -1,
-    19, -1, 20, -1, 1, 8, -1, 0, -1, 21, -1, 21, -1, 12, -1, 0, 0, -1, 21, 1,
-    14, 18, 0, -1, 14, 13, 13, 15, 5, 14
-]
-
-
-var loadVideo = function(i, continuer) { //cont=2 pour juste lire l'intro et la vidéo puis s'arrêter
-    if (activeTeam && continuer != 1) {
-        loadTeam(i);
-        return;
-    }
-    var offset = [activeIntro && hasIntro.indexOf(i) > -1 ? 1 : 0];
-
-    if (continuer == 1) {
-        window.location = 'video.html?id=' + (baseNumeroVideos + i - offset) + "&cont=1&origin=dvd-2&menu=-1";
-    } else {
-        window.location = 'video.html?id=' + (baseNumeroVideos + i - offset) + "&cont=" + (2 * offset) + "&origin=dvd-2&menu=" + currentMenu;
-    }
-}
-
-
-var randomize = function(i) {
-    if (i == 1) {
-        var pageRandom = Math.floor((Math.random() * 22));
-        goToTeam(pageRandom);
-    } else if (i == 2) {
-        var videoRandom = Math.floor((Math.random() * 60) + 1);
-        loadVideo(videoRandom, 0);
-    } else if (i == 3) {
-        $('.slideshowteam').cycle('destroy');
-        $(document).ready(function() {
-            $('.slideshowteam').cycle({
-                after: onAfterTeam,
-                slideResize: false,
-                containerResize: false,
-                fx: 'all',
-                speed: 500,
-                timeout: 0,
-                startingSlide: 0
-            });
-        });
-    } else {}
-}
-
-
 var accesPage = function(page) {
     window.location = page;
-}
-
-function switchIntro() {
-    activeIntro = !activeIntro;
-    if (activeIntro) {
-        document.getElementById("intro").src = "Images/intro_on.png";
-    } else {
-        document.getElementById("intro").src = "Images/intro.png";
-    }
 }
 
 
@@ -159,18 +96,6 @@ $(document).ready(function() {
     $('.slideshow').fadeIn();
 });
 
-var loadImage = function(i) {
-    var real = document.getElementById("photo_" + i).getAttribute("srcrl");
-    if (real != document.getElementById("photo_" + i).src) {
-        document.getElementById("photo_" + i).src = real;
-    }
-}
-
-var unloadAllImages = function() {
-    $('.lb-overlay img').each(function() {
-        $(this).attr('src', "");
-    });
-}
 
 
 $(document).ready(function() {
@@ -202,39 +127,13 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
-    $('.slideshowteam').cycle({
-        after: onAfterTeam,
-        slideResize: false,
-        containerResize: false,
-        fx: 'fade',
-        speed: 300,
-        timeout: 0,
-        startingSlide: currentTeam
-    });
-});
-
-
 function onBefore(curr, next, opts) {
     $('#prev')['hide']();
     $('#next')['hide']();
-    $('#goto0')['hide']();
-    $('#goto1')['hide']();
-    $('#goto2')['hide']();
-    $('#goto3')['hide']();
-    $('#goto4')['hide']();
-    $('#goto5')['hide']();
-    $('#introduction')['hide']();
 }
 
 function onAfter(curr, next, opts) {
     currentMenu = opts.currSlide;
-
-    for (i = 1; i <= 5; i++) {
-        var nom = "goto" + i;
-        document.getElementById(nom).getElementsByTagName('img')[0].src = "Images/bouton.png";
-    }
-    document.getElementById('goto' + (currentMenu + 1)).getElementsByTagName('img')[0].src = "Images/bouton_on.png";
 
 
     $('#prev')[currentMenu == 0 ? 'hide' : 'show'](appearTime);
--- source/data/videos.js
+++ source/data/videos.js
@@ -7,46 +7,50 @@ var videos = {
             "videoPathPrefix" : "../../../videos/2_Interviews/",
             "posterPathPrefix" : "../../../images/miniatures/",
             "videos" : [
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                },
-                {
-                    "poster" : "2-01_Jacques_Biot.png",
-                    "src" :  "1_Jacques_Biot.mp4",
-                    "title" : "Jacques Biot"
-                }
+                [
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    },
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    },
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    },
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    },
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    },
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    }
+                ],
+                [
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    },
+                    {
+                        "poster" : "2-01_Jacques_Biot.png",
+                        "src" :  "1_Jacques_Biot.mp4",
+                        "title" : "Jacques Biot"
+                    }
+                ]
             ]
         }
 }
