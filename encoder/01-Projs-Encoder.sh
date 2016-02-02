57c5b4f2f3d5cb8a489b67a573f4b2ac1edbdce0
Un mec dans le local JTX <jtxman@neo-jtx.eleves.polytechnique.fr>
2016-02-02 10:33:56 +0100

    Added encoding script for Vie de promo (minus the missing retrospectives)

--- encoder/01-Projs-Encoder.sh
+++ encoder/01-Projs-Encoder.sh
@@ -1,7 +1,7 @@
 #Each line is a call to the encoder
 #Parameter $1 is the folder to encode, $2 is the desired size of output videos in MB
-./APV-Encoder.sh ../01-Projs_JTX/2014-06-18_Proj_juin_JTX_2012 2815
-./APV-Encoder.sh ../01-Projs_JTX/2014-11-18_Proj_novembre_JTX_2012 2410
+./APV-Encoder.sh ../01-Projs_JTX/2014-06-18_Proj_juin_JTX_2012 2463
+./APV-Encoder.sh ../01-Projs_JTX/2014-11-18_Proj_novembre_JTX_2012 2109
 ./APV-Encoder.sh ../01-Projs_JTX/2015_02_18_Proj_fevrier_JTX_2013 3257
 ./APV-Encoder.sh ../01-Projs_JTX/2015-06-10_Proj_juin_JTX_2013 3997
 ./APV-Encoder.sh ../01-Projs_JTX/2015-11-12_Proj_novembre_JTX_2013 4689
