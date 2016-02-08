d5fef1d5c0d3b73ab2b59ae65248b138af234cad
Denis Merigoux <denis.merigoux@gmail.com>
2016-02-08 20:12:15 +0100

    Fonctionnal script to generate the menus

--- posters/APV-PosterExtractor.sh
+++ posters/APV-PosterExtractor.sh
@@ -1,32 +1,48 @@
 #!/bin/bash
-if [ -z $1 -o -z $2 ]; then
+###################################
+# Semi automatic APV menu generator
+###################################
+# Call it with APV-PosterExtractor.sh <projection folder>
+# For each video file in the folder, it plays the video,
+# then wait for you to enter the timestamp of the frame
+# you want as a poster. Then it asks the title of the video
+# and uses this information to create a ready-to-go JSON file
+# to be incorporated to APV 2013's video data file.
+
+if [ -z $1 ]; then
     exit 1
 fi
 
-
 #Parameters
 videos_folder=$1
-posters_folder=$2
+posters_folder=$1/posters
+json_file=$posters_folder/projection.json
 
-echo $videos_folder
-echo $posters_folder
+mkdir $posters_folder
+echo $(printf '[\n') >> $json_file
 
 #Deals with complex file names
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 #The first loop : encodes with constant quality
-for video in $(find "$videos_folder" -maxdepth 1 -type f -name "*.mp4")
+for video in $(find "$videos_folder" -maxdepth 1 -type f -name "*.mp4" | sort)
 do
     filename=$(basename $video)
-    poster="$posters_folder${filename%.mp4}.png"
-    echo $poster
+    poster="$posters_folder/${filename%.mp4}.png"
     nohup vlc $video &>/dev/null &
     read -p "Timestamp de la frame sous la forme mm:ss : " instant
-    ffmpeg -i "$video" -ss 00:$instant -vframes 1 -y "$poster"
+    ffmpeg -loglevel error -i "$video" -ss 00:$instant -vframes 1 -y "$poster"
     convert "$poster" -resize 250x444 "$poster"
+    read -p "Titre de la vidéo :" title
+    echo $(printf '{\r
+    "poster": "%s",\r
+    "src": "%s",\r
+    "title": "%s"\r
+}' $poster $video $title) >> $json_file
 done
 
+echo $(printf ']\n') >> $json_file
 
 #Restores filename dealing configuration
 IFS=$SAVEIFS
