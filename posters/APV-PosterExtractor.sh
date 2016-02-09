b7c22e03b571220cffd94e860cf6da066e1383eb
Denis Merigoux <denis.merigoux@gmail.com>
2016-02-09 08:58:40 +0100

    Fixed minor issues in extractor script

--- posters/APV-PosterExtractor.sh
+++ posters/APV-PosterExtractor.sh
@@ -27,19 +27,20 @@ IFS=$(echo -en "\n\b")
 
 #The first loop : encodes with constant quality
 for video in $(find "$videos_folder" -maxdepth 1 -type f -name "*.mp4" | sort)
-do
+do 
     filename=$(basename $video)
+    echo "Nom du fichier : "$filename
     poster="$posters_folder/${filename%.mp4}.png"
     nohup vlc $video &>/dev/null &
     read -p "Timestamp de la frame sous la forme mm:ss : " instant
     ffmpeg -loglevel error -i "$video" -ss 00:$instant -vframes 1 -y "$poster"
     convert "$poster" -resize 250x444 "$poster"
-    read -p "Titre de la vidéo :" title
+    read -p "Titre de la vidéo : " title
     echo $(printf '{\r
     "poster": "%s",\r
     "src": "%s",\r
     "title": "%s"\r
-}' $poster $video $title) >> $json_file
+},' $poster $video $title) >> $json_file
 done
 
 echo $(printf ']\n') >> $json_file
