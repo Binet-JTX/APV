2801fb06ff44d021ea1b31b197d566c0063f0c3c
Un mec dans le local JTX <jtxman@neo-jtx.eleves.polytechnique.fr>
2016-02-10 14:34:44 +0100

    Changed size of endoded videos due to corrected size of USB key

--- encoder/02-Vdp-Encoder.sh
+++ encoder/02-Vdp-Encoder.sh
@@ -1,21 +1,20 @@
 #Each line is a call to the encoder
 #Parameter $1 is the folder to encode, $2 is the desired size of output videos in MB
-./APV-Encoder.sh ../02-Vie_de_promo/2013-09-02_Incorpo_des_2k13/2013-09-03_Proj_de_mardi 1320
-./APV-Encoder.sh ../02-Vie_de_promo/2013-09-02_Incorpo_des_2k13/2013-09-07_Proj_de_samedi 1130
+./APV-Encoder.sh ../02-Vie_de_promo/2013-09-02_Incorpo_des_2k13/2013-09-03_Proj_de_mardi 880
+./APV-Encoder.sh ../02-Vie_de_promo/2013-09-02_Incorpo_des_2k13/2013-09-07_Proj_de_samedi 754
 ./APV-Encoder.sh ../02-Vie_de_promo/2013-09-09_Courtine_des_2k13/2013-09-23_WED 471
 ./APV-Encoder.sh ../02-Vie_de_promo/2013-09-09_Courtine_des_2k13/2013-09-26_Courtine 1421
-./APV-Encoder.sh ../02-Vie_de_promo/2014-04-15_Amphi_binets_des_2k13 2625
+./APV-Encoder.sh ../02-Vie_de_promo/2014-04-15_Amphi_binets_des_2k13 1969
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-05-03_WEI_des_2k13 449
-./APV-Encoder.sh ../02-Vie_de_promo/2014-06-09_Raid_de_l_X_2014 324
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-06_Campagne_CA 169
+./APV-Encoder.sh ../02-Vie_de_promo/2014-06-09_Raid_de_l_X_2014 216
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-09-01_Incorpo_des_2k14/2014-09-02_Proj_de_mardi 1296
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-09-01_Incorpo_des_2k14/2014-09-06_Proj_de_samedi 604
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-11-24_Campagne_Kes_des_2k13/2014-11-23_Amphi_des_listes_Kes 2261
-./APV-Encoder.sh ../02-Vie_de_promo/2014-11-24_Campagne_Kes_des_2k13/2014-11-27_Proj_jeudi 565
+./APV-Encoder.sh ../02-Vie_de_promo/2014-11-24_Campagne_Kes_des_2k13/2014-11-27_Proj_jeudi 377
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-11-24_Campagne_Kes_des_2k13/2014-12-01_Resultats_premier_tour 145
 ./APV-Encoder.sh ../02-Vie_de_promo/2014-11-24_Campagne_Kes_des_2k13/2014-12-04_Resultats_deuxieme_tour 304
 ./APV-Encoder.sh ../02-Vie_de_promo/2015-03-31_Amphi_binet_des_2k14 2108
-./APV-Encoder.sh ../02-Vie_de_promo/2015-05-16_Raid_de_l_X_2015 604
-./APV-Encoder.sh ../02-Vie_de_promo/2015-06-04_Proj_fete_des_langues 1044
-./APV-Encoder.sh ../02-Vie_de_promo/Khomiss 594
-./APV-Encoder.sh ../02-Vie_de_promo/2015-09-24_Courtine_des_2k15 1299
+./APV-Encoder.sh ../02-Vie_de_promo/2015-05-16_Raid_de_l_X_2015 403
+./APV-Encoder.sh ../02-Vie_de_promo/2015-06-04_Proj_fete_des_langues 522
+./APV-Encoder.sh ../02-Vie_de_promo/Khomiss 860
